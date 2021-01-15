1724ddd00bb37251675655790daaecda9db306be
Drew Thompson <drewallynthompson@gmail.com>
2021-01-15 07:24:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-let totalBatteries = (batteryBatches) => {
-  return batteryBatches.reduce((total, i) => i += total, 0);
+let reducer = (batteryBatches) => {
+  let totalBatteries = 0;
+  return batteryBatches.reduce((totalBatteries, i) => i += total, 0);
 };
